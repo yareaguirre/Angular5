c5c0df8be91c31162d19e8493328a18161ff10de
Yarely Aguirre <yareaguirre@gmail.com>
2018-03-01 12:54:28 -0600

    se agregan metodos de enrutamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { HeroService } from './hero.service';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
-import { AppRputingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
+
+import { AppRputingModule } from './/app-routing.module';
+
 
 
 @NgModule({
